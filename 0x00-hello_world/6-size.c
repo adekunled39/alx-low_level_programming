293cecff67cee295765d22357fbfbcdac39c0670
adekunled39 <adekunled39@gmail.com>
2022-04-07 18:27:17 -0700

    6th task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,4 +18,5 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 print("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 print("Size of a long long int: %lu byte(s)\n" (unsigned long)sizeof(c));
 print("Size of a float: %lu byte(s)\n" (unsigned long)sizeof(f);
+return (0);
 }
