56322490e4b92b13f52bd4c0240fe2dcc93a7903
adekunled39 <adekunled39@gmail.com>
2022-04-08 16:34:19 -0700

    my task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success/correction)
+ */
+int main(void)
+{
+char lowercase;
+char uppercase;
+
+for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+{
+putchar(lowercase);
+}
+
+for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+{
+putchar(uppercase);
+}
+
+putchar('\n');
+
+return (0);
+}
