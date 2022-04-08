3197844accf0a30ebbb6302a0276e0c4cfce351d
adekunled39 <adekunled39@gmail.com>
2022-04-08 13:34:33 -0700

    Task 0 for today

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+/* More headers goes there*/
+
+/* betty style for function main goes there*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n= rand() - RAND_MAX /2;
+	return (0);
+}
