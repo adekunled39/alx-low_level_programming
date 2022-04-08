05877e8e5b7f678e86039847e7bc6cc2c8cd92a2
adekunled39 <adekunled39@gmail.com>
2022-04-08 16:17:21 -0700

    Task three today

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+char game;
+for (game = 'a'; game <= 'z'; game++)
+{
+putchar(game);
+}
+
+putchar('\n');
+
+return (0);
+}
