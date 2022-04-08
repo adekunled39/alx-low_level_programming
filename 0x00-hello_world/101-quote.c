cc9182e3f2fab246181e3be8b573c330adeec91e
adekunled39 <adekunled39@gmail.com>
2022-04-07 20:40:29 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return: 0 is the rturn for successs or on
+ */
+int main(void)
+{write(1, "and that piece of art is useful\" -Dora korper, 2015-10-19", 59);
+return (1);
+}
