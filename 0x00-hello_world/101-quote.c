0df155106e0fe8128136ee3adc66138cc7035e2a
adekunled39 <adekunled39@gmail.com>
2022-04-07 20:46:45 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  * Return: 0 is the rturn for successs or on
  */
 int main(void)
-{write(1, "and that piece of art is useful\" -Dora korper, 2015-10-19", 59);
+{
+write(1, "and that piece of art is useful\" -Dora korper, 2015-10-19", 59);
 return (1);
 }
