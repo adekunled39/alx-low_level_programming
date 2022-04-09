eca322d6c8e1288aeaab2646f2a7fa0e2716fd6c
adekunled39 <adekunled39@gmail.com>
2022-04-08 17:07:19 -0700

    My number 4 task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdi.h>
+/**
+ * main - Entry point of code
+ *
+ * Return: Alwyaes return o for us 
+ */
+int main(void)
+{
+char game2;
+for (game2 = 'a'; game2 <= 'z'; game2++)
+{
+if (game2 == 'q')
+continue;
+
+else if (game2 == 'e')
+continue;
+
+putchar(game2);
+}
+
+putchar('\n');
+
+return (0);
