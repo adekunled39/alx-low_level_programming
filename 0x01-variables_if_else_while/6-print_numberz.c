8776d889ef8506d6e8dd612dd3f5ac4c446e031b
adekunled39 <adekunled39@gmail.com>
2022-04-08 18:38:21 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio>
+/**
+*main -Entry point
+*
+*Returne: Always 0 (success/correct)
+*/
+
+int main(void)
+{
+int i;
+for (i = 48 ; i <= 57; i++)
+{
+putchar(i);
+}
+putchar('\n');
+
+return (0);
+}
