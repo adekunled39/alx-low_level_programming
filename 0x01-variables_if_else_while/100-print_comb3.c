f873ebe4898decb1c0e400edee73903080615583
adekunled39 <adekunled39@gmail.com>
2022-04-08 21:08:52 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,10 +6,11 @@
 */
 
 int main(void)
+
 {
 int digit1, digit2;
 
-for (digit1 = 0 ; digit1 < 9; digit1++)
+for (digit1 = 0; digit1 < 9; digit1++)
 {
 for (digit2 = digit1 = + 1; digit2 < 10; digit2++)
 {
@@ -20,9 +21,11 @@ if (digit1 == 8 && digit2 == 9)
 continue;
 
 putchar(',');
+
 putchar(' ');
 }
 }
 
 printf('\n');
+
 return (0);
