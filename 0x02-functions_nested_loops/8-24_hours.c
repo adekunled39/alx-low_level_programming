c28141d9ff1b2d295876c31d64a82e214366fe37
adekunled39 <adekunled39@gmail.com>
2022-04-12 17:00:30 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-*jack_bower - Prints every minute of the day of
-*jack Bower, starting from 00:00 to 23:59.
+*jack_bauer - Prints every minute of the day of
+*jack Bauer, starting from 00:00 to 23:59.
 */
-void jack_bower(void)
+void jack_bauer(void)
 {
 int hour, minute;
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,7 +8,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jac_bower(void);
+void jac_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
