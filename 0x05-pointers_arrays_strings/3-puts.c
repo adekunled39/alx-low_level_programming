69fa7a9045712c925767db09c64c0c05a5586b24
adekunled39 <adekunled39@gmail.com>
2022-04-21 13:07:57 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,15 @@
-#include "main.h"                                                               
-
+#include "main.h"                                                        
 /**
-* _puts - prints a string
-* @str: pointer that will store the string's location
-* Return: nothing
+*_puts - prints a string
+*@str: pointer that will store the string's location
+*Return: nothing
 */
-
 void _puts(char *str)
-
 {
 while (*str != 0)
 {
 _putchar(*str);
-str++;										}
+str++;
+}
 _putchar(10);
 }
