2d061b763100fb0e9baa835475ee7981a5aecfd9
adekunled39 <adekunled39@gmail.com>
2022-04-23 14:09:00 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,4 @@
-#inlude "main.h"
+#include "main.h"
 
 /**
 * _strcmp - a function that compares two strings
@@ -6,7 +6,6 @@
 * @s2: input two
 * Return: Always 0 (Success)
 */
-
 int _strcmp(char *s1, char *s2)
 {
 int i;
