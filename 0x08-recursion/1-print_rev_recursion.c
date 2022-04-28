648986c0a7631947d59aa945f1582567ca9e6409
adekunled39 <adekunled39@gmail.com>
2022-04-28 15:24:24 -0700

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,9 +9,14 @@
 void _print_rev_recursion(char *s)
 
 {
-if (*s != '\0')
-_print_rev_recursion(s + 1);
-else
+
+if (*s == '\0')
+
+{
 return;
+}
+s++;
+_print_rev_recursion(s);
+s--;
 _putchar(*s);
 }
