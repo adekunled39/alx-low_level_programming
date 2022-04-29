d5e6bae82f66a1d0d66dd72d7ff03d1385a2a140
adekunled39 <adekunled39@gmail.com>
2022-04-28 17:48:15 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,14 +11,14 @@ void _puts_recursion(char *s)
 
 {
 
-if (*s == '\0')
+if (*s != '\0')
 
-return ('\n');
-
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
 else
 {
-return (*s);
-
-_puts_recursion(s + 1);
+_putchar('\n');
 }
 }
