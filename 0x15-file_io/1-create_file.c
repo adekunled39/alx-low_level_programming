b8c6b3d5c2f258a6d47ff1f2fe80af46e735b323
adekunled39 <adekunled39@gmail.com>
2022-06-02 12:51:40 -0700

    Task 1 updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,36 +1,43 @@
 #include "main.h"
 
 /**
- * create_file - a function that creates a file
+ * create_file - creates a file
+ * @filename: the file to read and write
+ * @text_content:  a NULL terminated string to write to the file
  *
- * @filename: name of file to create
- * @text_content: string to write to file
- *
- * Return: 1 on success OR -1 on faliure
-*/
+ * Return: 1 on succes or -1 on error
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int file, write_status, words = 0;
+int fd, len = 0, wc;
 
-	if (filename == NULL) /*check if filename is present*/
-		return (-1);
-
-	/*open file by creating it and if it exists write but truncate to 0*/
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (file == -1) /*check if file creation was a success*/
-		return (-1);
-
-	if (text_content) /*write content to file if its not NULL*/
-	{
-		while (text_content[words] != '\0') /*find number of words*/
-			words++;
-
-		/*write to file*/
-		write_status = write(file, text_content, words);
-		if (write_status == -1) /*check if write was a success*/
-			return (-1);
-	}
-
-	close(file); /*close file*/
-	return (1);
+if (filename == NULL)
+{
+return (-1);
+}
+if (text_content == NULL)
+{
+fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+if (fd < 0)
+{
+return (-1);
+}
+return (1);
+}
+fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+if (fd < 0)
+{
+return (-1);
+}
+while (text_content[len] != '\0')
+{
+len++;
+}
+wc = write(fd, text_content, len);
+if (wc < 0)
+{
+return (-1);
+}
+close(fd);
+return (1);
 }
